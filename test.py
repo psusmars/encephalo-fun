e9fb1a1cc13e23626a268a0ac4ae26b9eb75298e
Paul Susmarski <paul.susmarski@gmail.com>
2019-05-25 19:09:32 -0600

    include a sample data set for testing and update the test to start using it

--- test.py
+++ test.py
@@ -14,13 +14,78 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 
-names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
-dataset = pandas.read_csv("data/iris.csv", names=names)
+data_dir = "data/"
 
-# head
-print(dataset.head(20))
+# # first normalize the data
+# with  open(data_dir + "OpenBCI-RAW-1.1.1.1.a3.hal.txt", "r") as f:
+#   line = f.readline()
+#   while line.startswith("%"):
+#     line = f.readline()
+#   with open(data_dir + "in.csv","w+") as out_f:
+#     while line:
+#       out_f.write(line)
+#       line = f.readline()
+#   import pdb; pdb.set_trace()
+
+dataset = pandas.read_csv(data_dir + "in.csv", usecols=[1,2,3,4])
+
+# # head
+# print(dataset.head(20))
 
 
-# box and whisker plots
-dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
-plt.show()
+# # box and whisker plots
+# dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+# plt.show()
+
+# # histograms
+# dataset.hist()
+# plt.show()
+
+# # scatter plot matrix
+# scatter_matrix(dataset)
+# plt.show()
+
+# Split-out validation dataset
+array = dataset.values
+X = array[:,0:3]
+Y = array[:,3]
+import pdb; pdb.set_trace()
+validation_size = 0.20
+seed = 7
+scoring = 'accuracy'
+X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
+
+# Spot Check Algorithms
+models = []
+models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC(gamma='auto')))
+# evaluate each model in turn
+results = []
+names = []
+for name, model in models:
+	kfold = model_selection.KFold(n_splits=10, random_state=seed)
+	cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+	results.append(cv_results)
+	names.append(name)
+	msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+	print(msg)
+
+# # Compare Algorithms
+# fig = plt.figure()
+# fig.suptitle('Algorithm Comparison')
+# ax = fig.add_subplot(111)
+# plt.boxplot(results)
+# ax.set_xticklabels(names)
+# plt.show()
+
+# Make predictions on validation dataset
+knn = KNeighborsClassifier()
+knn.fit(X_train, Y_train)
+predictions = knn.predict(X_validation)
+print(accuracy_score(Y_validation, predictions))
+print(confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))
